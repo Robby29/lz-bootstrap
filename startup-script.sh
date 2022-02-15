7c17b2748f456d131b769b41fbd7f702c0ec424e
Robby <robbysingh@google.com>
2022-02-15 21:36:35 +0530

    minor changes

--- startup-script.sh
+++ startup-script.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 #check documenation on startup scripts on https://cloud.google.com/compute/docs/instances/startup-scripts/linux
-# user=`whoami`
-# echo "Logged in as user: $user" >> /startup-script-log
-# echo "installing go modules" >> /startup-script-log
-# runuser -l bootstrap -c "go get \"github.com/google/go-github/github\""
-# runuser -l bootstrap -c "go get \"golang.org/x/oauth2\""
-# runuser -l bootstrap -c "echo \"export GO111MODULE=off\" >> /home/bootstrap/.profile"
-# runuser -l bootstrap -c "source /home/bootstrap/.profile"
 runuser -l bootstrap -c "cd /home/bootstrap/lz-Bootstrap/landing-zone-automation/  && backend/backend &"
+runuser -l bootstrap -c "cd /home/bootstrap/lz-Bootstrap && git clone https://github.com/g-lz-automation/lz-ci-cd-jenkins.git"
+#runuser -l bootstrap -c "cd /home/bootstrap/lz-Bootstrap/lz-ci-cd-jenkins && terraform init && terraform apply -var-file terraform.tfvars -auto-approve"
