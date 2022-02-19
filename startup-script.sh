c2ee9ee1a7586f4c8d3265e914070809cd74e56f
Robby <robbysingh@google.com>
2022-02-19 12:58:34 +0530

    renoved integration with lz-ci-cd-jenkins.git

--- startup-script.sh
+++ startup-script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #check documenation on startup scripts on https://cloud.google.com/compute/docs/instances/startup-scripts/linux
-runuser -l bootstrap -c "cd /home/bootstrap/lz-Bootstrap/landing-zone-automation/  && backend/backend &"
-runuser -l bootstrap -c "cd /home/bootstrap/lz-Bootstrap && git clone https://github.com/g-lz-automation/lz-ci-cd-jenkins.git"
+runuser -l bootstrap -c "cd /home/bootstrap/lz-Bootstrap/lz-frontend-backend/  && backend/backend &"
+#runuser -l bootstrap -c "cd /home/bootstrap/lz-Bootstrap && git clone https://github.com/g-lz-automation/lz-ci-cd-jenkins.git"
 #runuser -l bootstrap -c "cd /home/bootstrap/lz-Bootstrap/lz-ci-cd-jenkins && terraform init && terraform apply -var-file terraform.tfvars -auto-approve"
