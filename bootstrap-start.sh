dd1fd6b880785326349faf44b0bd4caf7cefbb32
Robby <robbysingh@google.com>
2022-02-23 13:14:38 +0530

    path changes

--- bootstrap-start.sh
+++ bootstrap-start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #turn off go mudules
-cd ~
 git clone https://github.com/g-lz-automation/lz-frontend-backend.git
-cd ~/lz-frontend-backend/backend
 
 if [ -z "$GO111MODULE" ]; then
 echo "Setting env variable for go"
@@ -10,20 +8,20 @@ echo "export GO111MODULE=off" >> $HOME/.profile
 source $HOME/.profile
 fi
 #Install go packages
-cd ~/lz-frontend-backend/backend
+cd lz-frontend-backend/backend
 go get "github.com/google/go-github/github"
 go get "golang.org/x/oauth2"
 go build
 
 #Run Go server
-cd ~/lz-frontend-backend && backend/backend &
+cd ~/lz-bootsrap/lz-frontend-backend && backend/backend &
 
-cd ~/
+cd ~/lz-bootsrap/
 
 #Install Docker
 #sudo curl -fsSL https://get.docker.io | sh 
 sudo docker pull jenkins/jenkins:lts
-sudo docker build -t jenkins:custom -f /tmp/jenkins/Dockerfile ~/lz-bootstrap/jenkins/
+sudo docker build -t jenkins:custom -f ~/lz-bootstrap/jenkins/Dockerfile ~/lz-bootstrap/jenkins/
 
 sudo mv ~/lz-bootstrap/jenkins/start-jenkins.sh /bin/
 sudo mv ~/lz-bootstrap/jenkins/stop-jenkins.sh /bin/
